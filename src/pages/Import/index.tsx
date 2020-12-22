5b8b7a1ac981eb5bb2881edb25f19fac80f038f4
Hugo Arenari <hugoarenari@gmail.com>
2020-12-22 19:31:36 -0300

    done

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,29 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    if (uploadedFiles.length > 0) {
+      const data = new FormData();
 
-    // TODO
+      const { file } = uploadedFiles[0];
+      data.append('file', file);
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
+      try {
+        await api.post('/transactions/import', data);
+        history.push('/');
+      } catch (err) {
+        console.log(err.response.error);
+      }
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    setUploadedFiles(
+      files.map(file => ({
+        name: file.name,
+        readableSize: filesize(file.size),
+        file,
+      })),
+    );
   }
 
   return (
--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,8 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  const roundValue = value % 1 === 0;
+  return `R$ ${Intl.NumberFormat().format(Math.abs(value))}${
+    roundValue ? ',00' : ''
+  }`;
+};
 
 export default formatValue;
